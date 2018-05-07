d0e89d840b10d374fed3638306631eca5772fbd0
Graham Ollis <plicease@cpan.org>
2018-05-07 05:18:23 -0400

    Initial commit

---
+++ lib/Test2/Tools/JSON/Pointer.pm
@@ -0,0 +1,10 @@
+package Test2::Tools::JSON::Pointer;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Compare parts of JSON string to data structure using JSON pointers
+# VERSION
+
+1;
---
+++ t/test2_tools_json_pointer.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Test2::Tools::JSON::Pointer;
+
+ok 1, 'todo';
+
+done_testing
